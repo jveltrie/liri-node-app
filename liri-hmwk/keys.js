55d3faeb6571a2a2979e3bdf234e12d3d28b5e4f
jveltrie <jilljill68@gmail.com>
2019-04-23 23:23:35 -0700

    third time

---
+++ liri-hmwk/keys.js
@@ -0,0 +1,15 @@
+console.log('API KEYS ARE LOADED');
+
+exports.spotify = {
+  id: "fbdfba44af7844bfb92d318f3d986065",
+  secret: "3f103d07caad4819aa55b1d23ec07eae"
+};
+
+exports.omdb = {
+    id: "trilogy"
+};
+
+exports.bandsInTownAPI = {
+    id: "https://rest.bandsintown.com/artists/events?app_id=codingbootcamp"
+  };
+  
